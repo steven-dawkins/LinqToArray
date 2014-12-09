1358485dc34c2e74a8763897d2b6028dbd63f63c
Tim Stott <tstott@oxfordeconomics.com>
2014-12-09 09:23:13 +0000

    Added nuspec

--- src/LinqToArray/Properties/AssemblyInfo.cs
+++ src/LinqToArray/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApplication6")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("LinqToArray")]
+[assembly: AssemblyDescription("Specialised Linq-style operators for working with arrays")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApplication6")]
+[assembly: AssemblyProduct("LinqToArray")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
